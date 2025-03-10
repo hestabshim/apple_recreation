50beb1d0800ccde976d743fa5b3d811b880e4660
Amber <amberzeng@gmail.com>
2025-03-10 00:27:48 -0400

    navbar and macbar'

---
+++ src/components/section.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/section.module.css";
+const Section = () => {
+return (
+    <div className={`${styles["section"]}`}>
+        <h1>Mac</h1>
+        <h2>If you can dream it, Mac can do it.</h2>
+    </div>
+)
+}
+
+export default Section;
---
+++ src/pages/Mac.jsx
@@ -0,0 +1,6 @@
+import NavMacFamily from "../components/nav_macfamily";
+const Mac = () => {
+  return <NavMacFamily />;
+};
+
+export default Mac;
