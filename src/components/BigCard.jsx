da63a300bfe8b64115b5275b463374da2c977e8a
Amber <amberzeng@gmail.com>
2025-03-11 13:03:27 -0400

    added home page cardS

--- src/components/BigCard.jsx
+++ src/components/BigCard.jsx
@@ -1,17 +1,25 @@
 import styles from "../styles/home.module.css";
-const BigCard = () => {
+import PropTypes from "prop-types";
+
+const BigCard = ({ title, subtitle, cta }) => {
     return (
         <section className={`${styles.section} ${styles.dark}`}>
           <div className={`${styles.hero} ${styles.iphone15pro}`}>
-            <h2 className={styles.title}>iPhone 15 Pro</h2>
-            <h3 className={styles.subtitle}>Titanium. So strong. So light. So Pro.</h3>
+            <h2 className={styles.title}>{title}</h2>
+            <h3 className={styles.subtitle}>{subtitle}</h3>
             <div className={styles.cta}>
-              <a href="#" className={styles.link}>Learn more &gt;</a>
-              <a href="#" className={styles.link}>Buy &gt;</a>
+              <a href="#" className={styles.link}>{cta.link1}</a>
+              <a href="#" className={styles.link}>{cta.link2}</a>
             </div>
           </div>
         </section>
     )
+}
+
+BigCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    cta: PropTypes.object.isRequired
 }
 
 export default BigCard;
