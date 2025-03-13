9b15ec65440b586b44947966e4fab28a9823648c
Amber <amberzeng@gmail.com>
2025-03-13 15:52:11 -0400

    fix

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import GlobalNav from "./components/nav_global";
 import Mac from "./pages/Mac";
 import Home from "./pages/HomePage";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import Page404 from "./pages/404";
 const App = () => {
   return (
     <>
